813eb5f9b54672ef6d5f7cb36395f2d0584de8a2
Seth McCullough <seth@endevrs.dev>
2022-04-21 19:05:40 -0400

    Add matplotlib

--- index.py
+++ index.py
@@ -1,2 +1,8 @@
 import netCDF4
 import numpy as np
+import matplotlib.pyplot as plt
+
+f = netCDF4.Dataset('./xboutput.nc')
+
+sediment_transport_x = f.variables['Susg']
+print(sediment_transport_x.shape)
